d8ef2419212a8b24cf8d2a4dc35ccc3be34895cd
iremumut <irem2000.nur@gmail.com>
2022-03-03 22:24:23 +0300

    add restaurant dispatched

--- src/features/restaurant/restaurantsSlice.js
+++ src/features/restaurant/restaurantsSlice.js
@@ -14,6 +14,7 @@ const initialState = {
 export default function restaurantsReducer(state = initialState, action) {
   switch (action.type) {
     case "restaurants/add":
+      console.log(state);
       return {
         ...state,
         restaurants: [...state.restaurants, action.payload],
